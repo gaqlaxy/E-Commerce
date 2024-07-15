9c9288b5740afde91120295dc11f871e5c6a9ee8
gaqlaxy <v4vripper@gmail.com>
2024-07-15 14:45:45 +0530

    Added GSAP for Nav bar

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+// GSAP 
+
+var tl = gsap.timeline()
+
+tl.from(".gsap-logo", {
+    y: -25,
+    opacity: 0,
+    duration: 0.3,
+    delay: 0.5
+})
+
+tl.from(".gsap-nav", {
+    y: -25,
+    opacity: 0,
+    duration: 0.3,
+    stagger: 0.3
+})
