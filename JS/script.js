4cbd31ada146be17b133c585ec311911713953b5
gaqlaxy <v4vripper@gmail.com>
2024-07-15 15:44:44 +0530

    mODIFIED SCROLLBAR

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,14 @@
 
 var tl = gsap.timeline()
 
+
+// tl.from(".hero-heading", {
+//     y: -25,
+//     opacity: 0,
+//     duration: 0.3,
+//     // delay: 0.5
+// })
+
 tl.from(".gsap-logo", {
     y: -25,
     opacity: 0,
