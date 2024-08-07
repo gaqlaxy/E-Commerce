4b55def3b96eafe15dd1ad127a5de9518b03ce86
gaqlaxy <v4vripper@gmail.com>
2024-08-07 09:59:27 +0530

    Modified CSS

--- JS/script.js
+++ JS/script.js
@@ -89,3 +89,19 @@ function removeActiveClasses(){
         panel.classList.remove("active");
     })
 }
+
+// SWIPER 
+const swiper = new Swiper('.js-testimonials-slider', {
+    grabCursor: true,
+    spaceBetween: 30,
+    // loop: true,
+    pagination: {
+      el: '.js-testimonials-pagination',
+      clickable: true
+    },
+    breakpoints: {
+      767:{
+        slidesPerView: 2
+      }
+    }
+  });
